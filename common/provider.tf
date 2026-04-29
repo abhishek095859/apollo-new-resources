25b7d5b4d855950cc8c2db5be02c1256eb399e36
abhishek095859 <abhishek.narmula@minfytech.com>
2026-04-29 19:05:55 +0530

    Fix formatting of S3 bucket name in provider.tf

--- common/provider.tf
+++ common/provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket = "test-apollo-statefile-storing "
+    bucket = "test-apollo-statefile-storing"
     key    = "common_resources/terraform.tfstate"
     region = "ap-south-1"
         assume_role = {
