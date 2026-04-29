20f56bc2b0ee2b28b56307b6780d43b29a06fcbf
Chirumamella Subodh Chowdary <subodh.chowdary@minfytech.com>
2026-04-29 16:43:03 +0530

    creating KMS key for encryption

--- common/provider.tf
+++ common/provider.tf
@@ -5,7 +5,7 @@ terraform {
   backend "s3" {
     bucket = "atl-apac-mum-nw-cloud-iaac-s3"
     key    = "Terraform/NONSAPProd/Common/common_resources.tfstate"
-    region = "eu-central-1"
+    region = "ap-south-1"
         assume_role = {
       role_arn = "arn:aws:iam::468429600765:role/ATL-APAC-MUM-NW-TERRAFORM-IAAC-IAM-ROLE"
     }
