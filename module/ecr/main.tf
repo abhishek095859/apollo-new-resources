261eb51c48d57ad9e4f1e93ac5ee163e73eb05e0
abhishek095859 <abhishek.narmula@minfytech.com>
2026-04-30 20:09:39 +0530

    Update main.tf

--- module/ecr/main.tf
+++ module/ecr/main.tf
@@ -1,1 +1,14 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.repository_name
+  image_tag_mutability = var.image_tag_mutability
 
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+    encryption_configuration {
+      encryption_type = "KMS"
+      kms_key = var.kms_key_arn
+    }
+
+  tags = var.tags
+}
