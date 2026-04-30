48c2ebe0a68f762273d2d640eedc363d90471420
abhishek095859 <abhishek.narmula@minfytech.com>
2026-04-30 19:07:02 +0530

    Update outputs.tf

--- proj1/prod/outputs.tf
+++ proj1/prod/outputs.tf
@@ -8,35 +8,16 @@ output "ec2_private_ips" {
   value       = { for k, v in module.ec2_instances : k => v.instance_private_ip }
 }
 
+# UPDATE THIS BLOCK
 output "ec2_ami_used" {
-  description = "Windows Server 2022 AMI ID resolved at apply-time"
-  value       = data.aws_ami.windows_2022.id
+  description = "Windows Server 2019 AMI ID resolved at apply-time"
+  value       = data.aws_ami.windows_2019.id
 }
-
-# output "alb_dns_name" {
-#   description = "DNS name of the internal ALB"
-#   value       = module.alb.alb_dns_name
-# }
-
-# output "alb_arn" {
-#   description = "ARN of the internal ALB"
-#   value       = module.alb.alb_arn
-# }
-
-# output "alb_target_group_arn" {
-#   description = "ARN of the ALB target group"
-#   value       = module.alb.target_group_arn
-# }
 
 output "security_group_ids" {
   description = "Security group IDs created"
   value       = { for k, v in module.security_groups : k => v.security_group_id }
 }
-
-#output "kms_key_arn" {
-#  description = "KMS key ARN used for EBS encryption"
-#  value       = module.kms.kms_key_arn
-#}
 
 output "kms_key_arn" {
   description = "KMS key ARN retrieved from existing common resources"
